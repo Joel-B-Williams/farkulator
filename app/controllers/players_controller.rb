882c8950b142f3a0ec1ee70645c81516eec017b1
Joel Williams <joel.b.williams.is@gmail.com>
2019-03-05 21:36:22 -0600

    clean player update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,13 @@
 class PlayersController < ApplicationController
   def update
-    player = Player.find_by(id: params[:game_id])
+    player = Player.find_by(id: params[:id])
     game = player.game
     player.score += game.running_total
     game.running_total = 0
     if player.save && game.save
       redirect_to game_path(game)
     else
-      p "*" * 20
-      p "whoop"
+      flash[:warning]="Something has gone horribly pear shaped in players_controller ln 10"
     end
   end
 end
